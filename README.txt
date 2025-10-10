648ee3b60a1cd485ab4da3d74591201e51593eae
unknown <lokyung.chan@ontariotechu.net>
2025-10-10 15:48:24 -0400

    Added README.txt with suggested linear regression model

---
+++ README.txt
@@ -0,0 +1,13 @@
+The dataset describe the SOH, SOE and Q of 67 battery packs at different state of charge.
+
+What values are changing:
+
+Measured Capacity(doesn't change all), State of Charge, State of Energy, Cell's Voltage Reading, SOH(doesn't change at all for each battery pack)
+
+Relation:
+1. SOC vs SOH sort by SOC value
+2. SOE vs SOH : sort by SOE value
+3. Q vs SOH : sort by measured capacity
+4. V(with SOC, SOE, Q value) vs SOH : sort by battery id
+5. Combine all model
+
--- soh_model.py
+++ soh_model.py
@@ -1,4 +1,6 @@
 import pandas as pd #tool for opening excel file
+import random
+from sympy import symbols, integrate
 
 # load dataaset
 data = pd.read_excel('PulseBat Dataset.xlsx')
@@ -9,18 +11,44 @@ test_data = []
 
 def main():
     read_data();
-    split_data();
+    analysis_measured_charge();
 
 def read_data():
     for i, row in data.iterrows(): #loop through the rows
         row_data = [];
-        for j in range(7, len(data.columns)): # loop through the value from U1 to SOH
-            row_data.append(float(data.iloc[i, j]));  #insert value of one battery at a time
+        for j in range(0, len(data.columns)): # loop through the value from U1 to SOH
+            try:
+                row_data.append(float(data.iloc[i, j]));  #insert value of one battery at a time
+            except (TypeError, ValueError):
+                row_data.append(data.iloc[i, j]);
+
         raw_data.append(row_data); #insert the set of values at a time into the data pool
 
 def split_data():
-    quota = len(raw_data)*0.2;
-    #print(f"Test data quota: {quota}");
+    quota = int(len(raw_data)*0.2); #number of dataset for testing
+    
+    #first run for random selection
+    for i in range (0,len(raw_data)):
+        chance = random.random();
+        if chance <= 0.3 and quota > 0:
+            test_data.append(raw_data[i]);
+            quota -= 1;
+        else:
+            train_data.append(raw_data[i]);
+    
+    # second run if the test_data is not exactly 20% of the original data pool(134)
+    if quota > 0:
+        need = 134 - len(test_data); #the number of dataset need to fill up the the test data pool
+        
+        #loop through the train data pool
+        for i in range(0,need): 
+            index = random.randint(0, len(train_data)-1);  #pick a random index 
+            test_data.append(train_data.pop(index)); #move the data from train pool to test pool
+
+def analysis_measured_charge():
+        for i in range(0,len(raw_data)):
+            if raw_data[i][1] == 1.0:
+                print (raw_data[i]);
 
 
 # Call the main function
